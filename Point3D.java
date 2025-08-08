06decbc34e2480954c9d053ef136ae46b2b4a105
Ayesha Hassan Ansari <ansariayisha097@gmail.com>
2025-08-08 18:02:41 +0500

    PenPointClass

---
+++ Point3D.java
@@ -0,0 +1,30 @@
+package Week02;
+
+
+// Represents a point in 3D space by extending the 2D point (Week02.Point2D)
+public class Point3D extends Point2D {
+
+    // Additional coordinate for the third dimension (z-axis)
+    private int z;
+
+    // Constructor to initialize x, y (from Week02.Point2D) and z
+    public Point3D(int x, int y, int z) {
+        super(x, y);  // Call parent constructor to set x and y
+        this.z = z;
+    }
+
+    // Getter method to access z-coordinate
+    public int getZ() {
+        return z;
+    }
+
+    // Setter method to update z-coordinate
+    public void setZ(int z) {
+        this.z = z;
+    }
+
+    public String toString() {
+        return "Point ( " + getX() + ", " + getY() + ", " + getZ() + ")";
+    }
+
+}
